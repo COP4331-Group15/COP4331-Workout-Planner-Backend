9897db31ad75bdfd797c3c4ca88faf695c0ede9c
Timothy Golio <TimothyVGolio@gmail.com>
2021-07-06 12:17:56 -0400

    Adds create exercise endpoint

--- api.js
+++ api.js
@@ -1,8 +1,31 @@
 const FBEndpoints = require('./utils/firebase-endpoints');
 
+const axios = require('axios');
+
+const FB_URL = "https://cop4331-group15-default-rtdb.firebaseio.com";
+
 exports.setApp = function (app) {
+    app.post('/api/exercise/:uuid/create', async (req, res, next) => {
+
+      const { muscleGroup, focusTypes, name, sets, repititions, duration, resistance } = req.body;
+      const newExercise = {MuscleGroup:muscleGroup,Name:name,Sets:sets,Repititions:repititions,Duration:duration,Resistance:resistance}
+
+      var path = 'Exercises/' + req.params.uuid;
+      var ret;
+      var error = '';
+      try {
+        var builtPath = `${FB_URL}${path}.json`;
+        const result = await axios.post(builtPath, newExercise);
+        ret = {message: "Exercise created successfully", data: result};
+      } catch (e) {
+        error = e.toString();
+        ret = {message: "Error creating exercise", error: error};
+      }
+
+      re.status(200).json(ret);
+    })
     app.get('/api/test', async (req, res, next) => {
-        
+
         // Check our authentication status
         const auth = req.currentUser;
         const token = req.authToken;
@@ -15,7 +38,7 @@ exports.setApp = function (app) {
         } else {
             ret = {message: "Not Authenticated!"}
         }
-        
+
         // (res)olve our query with a 200 (OK) status, returning a JSON object (ret)
         res.status(200).json(ret);
     })
