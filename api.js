7685d8f7bea0072aca49c799256d73172bca4efe
Timothy Golio <TimothyVGolio@gmail.com>
2021-07-07 11:57:53 -0400

    Adds all API endppoints

--- api.js
+++ api.js
@@ -1,29 +1,272 @@
 const FBEndpoints = require('./utils/firebase-endpoints');
 
-const axios = require('axios');
-
-const FB_URL = "https://cop4331-group15-default-rtdb.firebaseio.com";
-
 exports.setApp = function (app) {
+
+    // ==================
+    // Exercise endpoints.
+    // ==================
+
+    // Creates an exercise.
     app.post('/api/exercise/:uuid/create', async (req, res, next) => {
 
-      const { muscleGroup, focusTypes, name, sets, repititions, duration, resistance } = req.body;
-      const newExercise = {MuscleGroup:muscleGroup,Name:name,Sets:sets,Repititions:repititions,Duration:duration,Resistance:resistance}
+      // Stores the inputted exercise in JSON format.
+      const {muscleGroup, focusTypes, name, sets, repititions, duration, resistance} = req.body;
+      const newExercise = {MuscleGroup: muscleGroup, Name: name, Sets: sets, Repititions: repititions, Duration: duration, Resistance: resistance};
+      const token = req.authToken;
 
-      var path = 'Exercises/' + req.params.uuid;
+      // Gets the path of the current user's exercises.
+      var path = 'exercise/' + req.params.uuid;
       var ret;
       var error = '';
       try {
-        var builtPath = `${FB_URL}${path}.json`;
-        const result = await axios.post(builtPath, newExercise);
+        // Attempts to post the JSON exercise to the database.
+        const result = await FBEndpoints.postValueAtPath(token, path, newExercise);
         ret = {message: "Exercise created successfully", data: result};
+        res.status(200).json(ret);
       } catch (e) {
+        // Prints any error that occurs.
         error = e.toString();
         ret = {message: "Error creating exercise", error: error};
+        res.status(400).json(ret);
       }
-
-      re.status(200).json(ret);
     })
+
+    // Gets all exercises for a given user.
+    app.get('/api/exercise/:uuid', async (req, res, next) => {
+
+      const token = req.authToken;
+
+      // Gets the path of the current user's exercises.
+      var path = 'exercise/' + req.params.uuid;
+      var ret;
+      try {
+        // Attempts to get all the JSON exercises at the current path.
+        const result = await FBEndpoints.getValueAtPath(token, path);
+        res.status(200).json(result);
+      } catch (e) {
+        // Prints any error that occurs.
+        ret = {message: "No user exists with that given ID"};
+        res.status(404).json(ret);
+      }
+    })
+
+    // Gets a specific exercise.
+    app.get('/api/exercise/:uuid/:exerciseId', async (req, res, next) => {
+
+      const token = req.authToken;
+
+      // Gets the path of the a specific exercise from among the user's exercises.
+      var path = 'exercise/' + req.params.uuid + '/' + req.params.exerciseId;
+      var ret;
+      var error = '';
+      try {
+        // Attempts to get the specific JSON exercise selected.
+        const result = await FBEndpoints.getValueAtPath(token, path);
+        res.status(200).json(result);
+      } catch (e) {
+        // Prints an error if the exercise is not found.
+        error = e.toString();
+        ret = {message: "Exercise not found", error: error};
+        res.status(404).json(ret);
+      }
+    })
+
+    // Updates an existing exercise.
+    app.patch('/api/exercise/:uuid/:exerciseId/update', async (req, res, next) => {
+
+      // Stores the inputted updated exercise in JSON format.
+      const {muscleGroup, focusTypes, name, sets, repititions, duration, resistance} = req.body;
+      const updatedExercise = {MuscleGroup: muscleGroup, Name: name, Sets: sets, Repititions: repititions, Duration: duration, Resistance: resistance}
+      const token = req.authToken;
+
+      // Gets the path of the a specific exercise from among the user's exercises.
+      var path = 'exercise/' + req.params.uuid + '/' + req.params.exerciseId;
+      var ret;
+      var error = '';
+      try {
+        // Attempts to put the updated exercise in the place of the selected exercise.
+        const result = await FBEndpoints.putValueAtPath(token, path, updatedExercise);
+        ret = {message: "Exercise updated successfully"};
+        res.status(200).json(ret);
+      } catch (e) {
+        // Prints any error that occurs.
+        error = e.toString();
+        ret = {message: "Error updateing exercise", error: error};
+        res.status(400).json(ret);
+      }
+    })
+
+    // ==================
+    // Workout endpoints
+    // ==================
+
+    // Creates a workout.
+    app.post('/api/workout/:uuid/create', async (req, res, next) => {
+
+      // Stores the inputted workout in JSON format.
+      const {muscleGroup, focusTypes, name, sets, repititions, duration, resistance, exercises} = req.body;
+      const newWorkout = {MuscleGroup: muscleGroup, Name: name, Sets: sets, Repititions: repititions, Duration: duration, Resistance: resistance, Exercises: exercises};
+      const token = req.authToken;
+
+      // Gets the path of the current user's workouts.
+      var path = 'workout/' + req.params.uuid;
+      var ret;
+      var error = '';
+      try {
+        // Attempts to post the JSON workout to the database.
+        const result = await FBEndpoints.postValueAtPath(token, path, newWorkout);
+        ret = {message: "Workout created successfully", data: result};
+        res.status(200).json(ret);
+      } catch (e) {
+        // Prints any error that occurs.
+        error = e.toString();
+        ret = {message: "Error creating workout", error: error};
+        res.status(400).json(ret);
+      }
+    })
+
+    // Gets all workouts for a given user.
+    app.get('/api/workout/:uuid', async (req, res, next) => {
+
+      const token = req.authToken;
+
+      // Gets the path of the current user's workouts.
+      var path = 'workout/' + req.params.uuid;
+      var ret;
+      try {
+        // Attempts to get all the JSON exercises at the current path.
+        const result = await FBEndpoints.getValueAtPath(token, path);
+        res.status(200).json(result);
+      } catch (e) {
+        // Prints any error that occurs.
+        ret = {message: "No user exists with that given ID"};
+        res.status(404).json(ret);
+      }
+    })
+
+    // Gets a specific workout.
+    app.get('/api/workout/:uuid/:workoutID', async (req, res, next) => {
+
+      const token = req.authToken;
+
+      // Gets the path of the a specific workout from among the user's workouts.
+      var path = 'workout/' + req.params.uuid + '/' + req.params.workoutId;
+      var ret;
+      var error = '';
+      try {
+        // Attempts to get the specific JSON workout selected.
+        const result = await FBEndpoints.getValueAtPath(token, path);
+        res.status(200).json(result);
+      } catch (e) {
+        // Prints an error if the workout is not found.
+        error = e.toString();
+        ret = {message: "Workout not found", error: error};
+        res.status(404).json(ret);
+      }
+    })
+
+    // Updates an existing workout.
+    app.patch('/api/workout/:uuid/:workoutId/update', async (req, res, next) => {
+
+      // Stores the inputted updated workout in JSON format.
+      const {muscleGroup, focusTypes, name, sets, repititions, duration, resistance, exercises} = req.body;
+      const updatedWorkout = {MuscleGroup: muscleGroup, Name: name, Sets: sets, Repititions: repititions, Duration: duration, Resistance: resistance, Exercises: exercises}
+      const token = req.authToken;
+
+      // Gets the path of the a specific workout from among the user's workouts.
+      var path = 'workout/' + req.params.uuid + '/' + req.params.workoutId;
+      var ret;
+      var error = '';
+      try {
+        // Attempts to put the updated workout in the place of the selected workout.
+        const result = await FBEndpoints.putValueAtPath(token, path, updatedWorkout);
+        ret = {message: "Workout updated successfully"};
+        res.status(200).json(ret);
+      } catch (e) {
+        // Prints any error that occurs.
+        error = e.toString();
+        ret = {message: "Error updateing workout", error: error};
+        res.status(400).json(ret);
+      }
+    })
+
+    // ==================
+    // Split endpoints
+    // ==================
+
+    // Creates a split.
+    app.post('/api/split/:uuid/create', async (req, res, next) => {
+
+      // Stores the inputted split in JSON format.
+      const {focus, startDate, length, workouts} = req.body;
+      const newSplit = {Focus: focus, StartDate: startDate, Length: length, Workouts: workouts};
+      const token = req.authToken;
+
+      // Gets the path where the user's split will be stored.
+      var path = 'split/' + req.params.uuid;
+      var ret;
+      var error = '';
+      try {
+        // Attempts to post the JSON split to the database.
+        const result = await FBEndpoints.postValueAtPath(token, path, newSplit);
+        ret = {message: "Split created successfully", data: result};
+        res.status(200).json(ret);
+      } catch (e) {
+        // Prints any error that occurs.
+        error = e.toString();
+        ret = {message: "Error creating split", error: error};
+        res.status(400).json(ret);
+      }
+    })
+
+    // Gets the split for a given user.
+    app.get('/api/split/:uuid', async (req, res, next) => {
+
+      const token = req.authToken;
+
+      // Gets the path of the current user's split.
+      var path = 'split/' + req.params.uuid;
+      var ret;
+      try {
+        // Attempts to get the user's JSON split from the current path.
+        const result = await FBEndpoints.getValueAtPath(token, path);
+        res.status(200).json(result);
+      } catch (e) {
+        // Prints any error that occurs.
+        ret = {message: "No user exists with that given ID"};
+        res.status(404).json(ret);
+      }
+    })
+
+    // Updates an existing split.
+    app.patch('/api/split/:uuid/update', async (req, res, next) => {
+
+      // Stores the inputted updated workout in JSON format.
+      const {focus, startDate, length, workouts} = req.body;
+      const updatedSplit = {Focus: focus, StartDate: startDate, Length: length, Workouts: workouts};
+      const token = req.authToken;
+
+      // Gets the path of the current user's split.
+      var path = 'split/' + req.params.uuid;
+      var ret;
+      var error = '';
+      try {
+        // Attempts to put the updated split in the place of the user's current split.
+        const result = await FBEndpoints.putValueAtPath(token, path, updatedSplit);
+        ret = {message: "Split updated successfully"};
+        res.status(200).json(ret);
+      } catch (e) {
+        // Prints any error that occurs.
+        error = e.toString();
+        ret = {message: "Error updateing split", error: error};
+        res.status(400).json(ret);
+      }
+    })
+
+    // ==================
+    // Test endpoints
+    // ==================
+
     app.get('/api/test', async (req, res, next) => {
 
         // Check our authentication status
